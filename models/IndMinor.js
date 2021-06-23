e60be660e531e2cb7e2a425d4e3c4091b65f4f75
Bensho <57368018+bblinder62@users.noreply.github.com>
2021-06-23 11:13:30 -0500

    Initialize

---
+++ models/IndMinor.js
@@ -0,0 +1,14 @@
+
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var indMinorSchema = Schema( {
+  ownerId: ObjectId,
+  title: String,
+  description:String,
+  createdAt: Date,
+} );
+
+module.exports = mongoose.model( 'IndMinor', indMinorSchema );
