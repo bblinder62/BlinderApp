b7920f846134f1f6ee1cc14030dc7bdf9a3ff751
Bensho <57368018+bblinder62@users.noreply.github.com>
2021-07-01 08:18:56 -0500

    Create auth.sh

---
+++ config/auth.sh
@@ -0,0 +1,5 @@
+export MONGODB_URI="mongodb://localhost/authDemo"
+export clientID='331237148042-btmoaoftf8bua515rek0pqn5bu12m104.apps.googleusercontent.com'
+export clientSecret='17W8hvbgpq-giIogMTAKQlN9'
+export callbackURL='http://localhost:5000/login/authorized'
+nodemon
